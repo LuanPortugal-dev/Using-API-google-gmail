f22767b83e85e75fb9b49615a5ee639e4cd041fe
Luan Portugal <luanportugal.profissional.tec@gmail.com>
2021-01-03 23:41:54 -0300

    Adding dot env, determining whether it is opening and transferring

--- emailchecker.py
+++ emailchecker.py
@@ -1,9 +1,17 @@
 import email
 import imaplib
 import emailchecker
+import re
+import os
 
-EMAIL = 'luanportugal.profissional.tec@gmail.com'
-PASSWORD = 'amorloveyou'
+from pathlib import Path
+env_path = Path('.env')
+
+from dotenv import load_dotenv
+load_dotenv(dotenv_path=env_path)
+
+EMAIL = os.getenv("GMAIL_EMAIL")
+PASSWORD = os.getenv("GMAIL_PASSWORD")
 SERVER = 'imap.gmail.com'
 
 
@@ -11,7 +19,7 @@ SERVER = 'imap.gmail.com'
 def read_email_from_gmail():
     mail = imaplib.IMAP4_SSL(SERVER)
     mail.login(EMAIL, PASSWORD)
-    mail.select('inbox')
+    mail.select('Teste')
 
     status, data = mail.search(None, 'ALL')
 
@@ -44,10 +52,30 @@ def read_email_from_gmail():
                     mail_content = message.get_payload()
 
 
-                print('From: {}'.format(mail_from))
-                print('Subject: {}'.format(mail_subject))
-                print('Content: {}'.format(mail_content))
 
+                nome = 'Luan' 
+                codigo = '0000000'
+                cpf = '222222222'
+
+
+
+
+                # Diz se é abertura ou transferência
+                if mail_subject == 'Abertura de conta cliente':
+                    print(f'{nome} cadastrado como Abertura, com código {codigo} e cpf {cpf}')
+                elif 'Transferencia de cliente' or 'Transferencia de clientes':
+                    print(f'{nome} cadastrado como transferência, com código {codigo} e cpf {cpf}')
+                else:
+                    print("Nenhum tipo de dado válido")        
+
+
+                #arquivo = open("./texto.txt", "a")
+                #arquivo.write('From: {}'.format(mail_from))
+                #print('Subject: {}'.format(mail_subject))
+                #print('Content: {}'.format(mail_content))
+                
+
+                
 
 if __name__ == '__main__':
     read_email_from_gmail()
